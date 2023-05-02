2b73f00e569451159cc0672caae36a5d0d1eab9c
Willian Amancio <wvsamancio@gmail.com>
2023-05-01 21:42:54 -0300

    Added details components

---
+++ src/app/contribute-details/contribute-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contribute-details',
+  templateUrl: './contribute-details.component.html',
+  styleUrls: ['./contribute-details.component.css']
+})
+export class ContributeDetailsComponent {
+
+}
